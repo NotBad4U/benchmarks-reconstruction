f26a6ae26cbcdc27adb8cb0fb604a5990d48f703
NotBad4U <lightplay8@gmail.com>
2024-12-12 23:56:49 +0000

    Create infra for running test that do not need to be split

--- tests/spec_test.rs
+++ tests/spec_test.rs
@@ -1,65 +1,105 @@
-use std::panic::AssertUnwindSafe;
+use std::io;
+use std::io::BufRead;
+use std::path::PathBuf;
 use std::process::Command;
 use std::process::ExitStatus;
+use std::process::Stdio;
 
 use file_test_runner::collect_and_run_tests;
-use file_test_runner::collection::CollectedTest;
-use file_test_runner::collection::CollectOptions;
 use file_test_runner::collection::strategies::TestPerFileCollectionStrategy;
+use file_test_runner::collection::CollectOptions;
+use file_test_runner::collection::CollectedTest;
 use file_test_runner::RunOptions;
 use file_test_runner::TestResult;
 
-fn main() {
-  collect_and_run_tests(
-    CollectOptions {
-      base: "tests/benchs".into(),
-      strategy: Box::new(TestPerFileCollectionStrategy {
-       file_pattern: Some("^.*\\.alethe$".to_owned())
-      }),
-      filter_override: None,
-    },
-    RunOptions {
-      parallel: true,
-    },
-    // custom function to run the test...
-    |test| {
-      // * do something like this
-      // * or do some checks yourself and return a value like TestResult::Passed
-      // * or use `TestResult::from_maybe_panic_or_result` to combine both of the above
-      // TestResult::from_maybe_panic(AssertUnwindSafe(|| {
-        
-      // }))
-      let status =  run_test(test);
+use std::fs::File;
+use thiserror::Error;
 
-      if status.code().unwrap() == 0 {
-        TestResult::Passed
-      } else {
-        TestResult::Failed { output: vec![] }
-      }
-    }
-  )
+
+#[derive(Error, Debug)]
+enum ErrorBench {
+  #[error("Io erro")]
+  IoErr(io::Error),
+  #[error("Lambdapi check failed")]
+  LambdapiError,
+  #[error("Carcara failed to translate")]
+  CarcaraError,
 }
 
-// The `test` object only contains the test name and
-// the path to the file on the file system which you can
-// then use to determine how to run your test
-fn run_test(test: &CollectedTest) -> ExitStatus {
-  // Properties:
-  // * `test.name` - Fully resolved name of the test.
-  // * `test.path` - Path to the test file this test is associated with.
-  // * `test.data` - Data associated with the test that may have been set
 
-  let file_name = &test.name;
-  let alethe_file = test.path.clone();
-  let problem_file = test.path.with_extension("smt2");
 
-  let status = Command::new("carcara")
-    .arg("check")
-    .args(["--log", "off"])
-    .arg("-i")
-    .arg(alethe_file)
-    .arg(problem_file)
-    .status()
-    .expect(format!("failed running carcara check on {}", file_name).as_str());
-  status 
+fn main() {
+    collect_and_run_tests(
+        CollectOptions {
+            base: "tests/benchs".into(),
+            strategy: Box::new(TestPerFileCollectionStrategy {
+                file_pattern: Some("^.*\\.alethe$".to_owned()),
+            }),
+            filter_override: None,
+        },
+        RunOptions { parallel: true },
+        |test| {
+            if count_lines(&test.path) != 0 {
+              let res = run_test(test);
+
+              if let Ok(code) = res {
+                if code.code().unwrap() == 0 {
+                  TestResult::Passed
+                } else {
+                    TestResult::Failed { output: vec![] }
+                }
+              } else {
+                TestResult::Failed { output: vec![] }
+              }
+            } else {
+              TestResult::Ignored
+            }           
+        },
+    )
+}
+
+fn run_test(test: &CollectedTest) -> Result<ExitStatus, ErrorBench> {
+    // let file_name = &test.name;
+    let alethe_file = test.path.clone();
+    let problem_file = test.path.with_extension("smt2");
+
+    let tmpdir: PathBuf = PathBuf::from("tmp");
+    let lp_file_path = tmpdir.join(test.path.with_extension("lp").file_name().unwrap());
+
+    let file = File::create(lp_file_path.clone()).unwrap();
+
+    let status = Command::new("carcara")
+      .arg("translate")
+      .args(["--log", "off"])
+      .arg("--expand-let-bindings")
+      .arg("-i")
+      .arg(alethe_file)
+      .arg(problem_file)
+      .stdout(Stdio::from(file))
+      .stderr(Stdio::null())
+      .status()
+      .map_err(|e| ErrorBench::IoErr(e))
+      ?;
+
+    if status.code().unwrap() != 0 {
+      std::fs::remove_file(lp_file_path).unwrap();
+      return Err(ErrorBench::CarcaraError)
+    }
+
+    let status = Command::new("lambdapi")
+      .args(["check", "-v0", "-w", "--timeout=100"])
+      .arg(lp_file_path.clone())
+      .status()
+      .map_err(|e| ErrorBench::IoErr(e))
+      ?;
+
+    std::fs::remove_file(lp_file_path).unwrap();
+
+    Ok(status)
+}
+
+fn count_lines(path: &PathBuf) -> usize {
+    let lines = std::io::BufReader::new(std::fs::File::open(path).unwrap()).lines();
+    let line_count = lines.count();
+    line_count
 }
