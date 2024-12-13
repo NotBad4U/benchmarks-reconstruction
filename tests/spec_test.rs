334ce364f37281957a9e244e26bbdd66eb5ca626
NotBad4U <lightplay8@gmail.com>
2024-12-13 01:37:41 +0000

    Architecture to run monthread app

--- tests/spec_test.rs
+++ tests/spec_test.rs
@@ -15,18 +15,20 @@ use file_test_runner::TestResult;
 use std::fs::File;
 use thiserror::Error;
 
+use rev_buf_reader::RevBufReader;
+
+
+use regex::Regex;
 
 #[derive(Error, Debug)]
 enum ErrorBench {
-  #[error("Io erro")]
-  IoErr(io::Error),
-  #[error("Lambdapi check failed")]
-  LambdapiError,
-  #[error("Carcara failed to translate")]
-  CarcaraError,
+    #[error("Io erro")]
+    IoErr(io::Error),
+    #[error("Lambdapi check failed")]
+    LambdapiError,
+    #[error("Carcara failed to translate")]
+    CarcaraError,
 }
-
-
 
 fn main() {
     collect_and_run_tests(
@@ -39,21 +41,21 @@ fn main() {
         },
         RunOptions { parallel: true },
         |test| {
-            if count_lines(&test.path) != 0 {
-              let res = run_test(test);
+            if count_lines(&test.path) < 300 {
+                let res = run_test(test);
 
-              if let Ok(code) = res {
-                if code.code().unwrap() == 0 {
-                  TestResult::Passed
+                if let Ok(code) = res {
+                    if code.code().unwrap() == 0 {
+                        TestResult::Passed
+                    } else {
+                        TestResult::Failed { output: vec![] }
+                    }
                 } else {
                     TestResult::Failed { output: vec![] }
                 }
-              } else {
-                TestResult::Failed { output: vec![] }
-              }
             } else {
-              TestResult::Ignored
-            }           
+                TestResult::Ignored
+            }
         },
     )
 }
@@ -69,37 +71,46 @@ fn run_test(test: &CollectedTest) -> Result<ExitStatus, ErrorBench> {
     let file = File::create(lp_file_path.clone()).unwrap();
 
     let status = Command::new("carcara")
-      .arg("translate")
-      .args(["--log", "off"])
-      .arg("--expand-let-bindings")
-      .arg("-i")
-      .arg(alethe_file)
-      .arg(problem_file)
-      .stdout(Stdio::from(file))
-      .stderr(Stdio::null())
-      .status()
-      .map_err(|e| ErrorBench::IoErr(e))
-      ?;
+        .arg("translate")
+        .args(["--log", "off"])
+        .arg("--expand-let-bindings")
+        .arg("-i")
+        .arg(alethe_file)
+        .arg(problem_file)
+        .stdout(Stdio::from(file))
+        .stderr(Stdio::null())
+        .status()
+        .map_err(|e| ErrorBench::IoErr(e))?;
 
     if status.code().unwrap() != 0 {
-      std::fs::remove_file(lp_file_path).unwrap();
-      return Err(ErrorBench::CarcaraError)
+        std::fs::remove_file(lp_file_path).unwrap();
+        return Err(ErrorBench::CarcaraError);
     }
 
     let status = Command::new("lambdapi")
-      .args(["check", "-v0", "-w", "--timeout=100"])
-      .arg(lp_file_path.clone())
-      .status()
-      .map_err(|e| ErrorBench::IoErr(e))
-      ?;
+        .args(["check", "-v0", "-w", "--timeout=10"])
+        .arg(lp_file_path.clone())
+        .status()
+        .map_err(|e| ErrorBench::IoErr(e))?;
 
-    std::fs::remove_file(lp_file_path).unwrap();
+    //std::fs::remove_file(lp_file_path).unwrap();
 
     Ok(status)
 }
 
 fn count_lines(path: &PathBuf) -> usize {
-    let lines = std::io::BufReader::new(std::fs::File::open(path).unwrap()).lines();
-    let line_count = lines.count();
-    line_count
+    //let lines = std::io::BufReader::new().lines();
+    let buf = RevBufReader::new(std::fs::File::open(path).unwrap());
+    let mut lines = buf.lines();
+    lines.next();
+  
+    let last = lines.next().unwrap().unwrap();
+
+    let re = Regex::new(r"t(\d+)").unwrap();
+
+    let num = re
+        .captures(last.as_str())
+        .map(|cap| cap.get(1).map_or(0, |m| m.as_str().parse::<usize>().unwrap()))
+        .unwrap();
+    num
 }
